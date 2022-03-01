ff578145a4dca4f0fac2b5bab2af84fc241845f4
M. Droste <m.droste@mdwp.de>
2022-03-01 17:47:18 +0100

    env

--- app/root.tsx
+++ app/root.tsx
@@ -25,8 +25,8 @@ export const meta: MetaFunction = () => {
 }
 
 export const loader = () => {
-  const apiKey = process.env.API_KEY
-  const appId = process.env.APP_ID
+  const apiKey = "c5712074-2b62-4fa2-8bd7-e7f98d8702bf"
+  const appId = "b863c797-91dd-4e19-939b-3fa2ccf28bf6"
 
   if (!apiKey || !appId) {
     throw new Error('Missing React Bricks credentials in .env file')
