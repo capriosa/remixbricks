6591c26ee7088d679532f6d153f1422bb7a5394f
M. Droste <m.droste@mdwp.de>
2022-03-02 13:04:53 +0100

    hero changes

--- app/react-bricks/bricks/MdwpHeroUnit.tsx
+++ app/react-bricks/bricks/MdwpHeroUnit.tsx
@@ -29,21 +29,17 @@ const MdwpBgHeroUnit: types.Brick<HeroUnitProps> = ({ background, color, bgColor
     
     className={`w-full flex items-center px-6 pb-12 antialiased bg-cover  
     
-    
     ${
       fullHeight ? 'min-h-screen' : ''
     }`}>
       
   
-    
-  
-
-    
     <div className={`mx-auto max-w-7xl `}>
     <div className="container max-w-lg px-4 py-32 mx-auto text-left md:max-w-none md:text-center">
         <Text
           renderBlock={(props) => (
-            <h1 className={`text-5xl font-extrabold leading-10 tracking-tight text-${headlineAlignment} ${
+            <h1 className={`text-5xl font-extrabold leading-10 tracking-tight text-${headlineAlignment} 
+            ${
               bgColor.className
             } sm:leading-none md:text-6xl lg:text-7xl
             ${color?.className}
@@ -56,7 +52,10 @@ const MdwpBgHeroUnit: types.Brick<HeroUnitProps> = ({ background, color, bgColor
         />
         <RichText
           renderBlock={(props) => (
-            <p className={`mx-auto mt-5 ${paragraphColor?.className} text-${paragraphAlignment} md:mt-12 md:max-w-lg lg:text-lg`}>
+            <p className={`mx-auto mt-5 ${paragraphColor?.className} 
+            text-${paragraphAlignment} 
+            
+            md:mt-12 md:max-w-lg lg:text-lg`}>
               {props.children}
             </p>
           )}
@@ -76,7 +75,18 @@ const MdwpBgHeroUnit: types.Brick<HeroUnitProps> = ({ background, color, bgColor
           )}
         />
         </div>
-      </div>
+        <div className="flex flex-col items-center mt-12 text-center">
+                <span className="relative inline-flex w-full md:w-auto">
+                    <a href="#_" type="button" className="inline-flex items-center justify-center w-full px-8 py-4 text-base font-bold leading-6 text-gray bg-orange border border-transparent rounded-full md:w-auto hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600">
+                        Purchase Now
+                    </a>
+                    <span className="absolute top-0 right-0 px-2 py-1 -mt-12 -mr-6 text-xs font-medium leading-tight text-white bg-light-brown rounded-full">only $15/mo</span>
+                </span>
+                <a href="#" className="mt-12 text-sm text-orange">Learn More</a>
+            </div>
+        </div>
+      
+      
     </section>
   )
 }
